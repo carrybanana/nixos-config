49db2b7518848641e917e5474996041562a58a8a
carrybanana <maomaoyeah520@163.com>
2026-04-11 23:06:24 +0800

    update

--- home/carry/hyprland/default.nix
+++ home/carry/hyprland/default.nix
@@ -1,10 +1,9 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-  # 只管理配置文件,允许双向编辑：改 ~/.config 会同步回 /etc/nixos
+  # 只管理配置文件
   home.file.".config/hypr/hyprland.conf" = {
     source = ./hyprland.conf;
-    readable = true;
-    writable = true;   # 允许你直接编辑 ~/.config 里的文件
+    executable = false;  # 可写文件不需要执行权限
   };
 }
