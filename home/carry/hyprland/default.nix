d57611cf7fc0a86b90f29c785caa1e09c37bc37e
carrybanana <maomaoyeah520@163.com>
2026-04-11 23:01:45 +0800

    update

--- home/carry/hyprland/default.nix
+++ home/carry/hyprland/default.nix
@@ -1,6 +1,10 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-  # 只管理配置文件，别的什么都不做
-  xdg.configFile."hypr/hyprland.conf".source = ./hyprland.conf;
+  # 只管理配置文件,允许双向编辑：改 ~/.config 会同步回 /etc/nixos
+  home.file.".config/hypr/hyprland.conf" = {
+    source = ./hyprland.conf;
+    readable = true;
+    writable = true;   # 允许你直接编辑 ~/.config 里的文件
+  };
 }
--- home/carry/niri/default.nix
+++ home/carry/niri/default.nix
@@ -1,6 +1,10 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-  # 只管理配置文件
-  xdg.configFile."niri/config.kdl".source = ./config.kdl;
+  # 只管理配置文件,允许双向编辑：改 ~/.config 会同步回 /etc/nixos
+  home.file.".config/niri/config.kdl" = {
+    source = ./config.kdl;
+    readable = true;
+    writable = true;      # 允许你直接编辑 ~/.config 里的文件
+  };
 }
