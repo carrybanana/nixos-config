2c3ff63ad0ce665f569ff3a759a78943748a0ca8
carrybanana <maomaoyeah520@163.com>
2026-05-03 22:08:56 +0800

    update flake

--- modules/default.nix
+++ modules/default.nix
@@ -26,9 +26,8 @@
     ];
     # 仅保留官方公钥（清华/中科大无独立公钥）
     trusted-public-keys = [
-      # 官方源的公钥
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="  # 官方源的公钥
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="   # nix-community公钥
     ];
 
     # 关键：添加可信用户
