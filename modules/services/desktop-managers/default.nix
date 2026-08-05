ec7c371107d12fa8b78444c699e31677b3821c93
carrybanana <maomaoyeah520@163.com>
2026-08-05 22:20:05 +0800

    hyprland

--- modules/services/desktop-managers/default.nix
+++ modules/services/desktop-managers/default.nix
@@ -3,8 +3,8 @@
 {
   # 导入模块
   imports = [
-#     ./hyprland/default.nix
-    ./niri/default.nix
+    ./hyprland/default.nix
+#     ./niri/default.nix
 #     ./cosmic.nix
 #     ./gnome.nix
 #     ./kde-plasma.nix
